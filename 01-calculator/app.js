341f0afa448687d8d5729f3052aa6449fb496b54
md sarwar khan <khansarwar4402@gmail.com>
2026-08-28 22:25:00 +0530

    Add calculator project

---
+++ 01-calculator/app.js
@@ -0,0 +1,53 @@
+const display = document.querySelector('input');
+const buttons = document.querySelectorAll('.buttons button');
+
+let exp = "";
+const OPERATORS = ["+","*","-","/","%"];
+
+buttons.forEach(button => {
+    button.addEventListener('click',() => {
+        const value = button.dataset.value;
+        if(!value)return;
+
+        if(value === "AC") {
+            exp = "";
+            display.value = "";
+            return;
+        }
+
+        if(value === "DEL") {
+            exp = exp.slice(0,-1);
+            display.value = exp;
+            return;
+        }
+
+        if(value === "=") {
+            if(exp === "" || OPERATORS.includes(exp.slice(-1)))
+            {
+                display.value = "Error";
+                exp = "";
+                return;
+            }
+            try{
+                exp = eval(exp).toString();
+                display.value = exp;
+            }
+            catch {
+                display.value = "Error";
+                exp = "";
+            }
+            return;
+        }
+        if(OPERATORS.includes(value) && exp === ""){
+            return;
+        }
+
+
+        if(OPERATORS.includes(value) && OPERATORS.includes(exp.slice(-1))){
+            return;
+        }
+        
+        exp += value;
+        display.value = exp;
+    });
+});
